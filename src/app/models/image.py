96aeef672eb082df74a65ed08b811e4f1030287e
OPX <shaban@opx.slmail.me>
2024-03-18 19:51:26 +0100

    [🐛 FIXED:FEAT] Improve features model and image utils
    REFERENCES: [Face](https://trello.com/c/mNouDMWu)

--- src/app/models/image.py
+++ src/app/models/image.py
@@ -1,14 +1,14 @@
 from pydantic import BaseModel, validator
-import io
-from PIL import Image
+import numpy as np
+import cv2
 
 from src.app.core.enums import ImageFormatEnum
 
 
 class ImageModel(BaseModel):
     filename: str
-    modified_filename: str | None
     data: bytes
+    modified_filename: str | None
     format: ImageFormatEnum
 
     @validator("format")
@@ -16,3 +16,10 @@ class ImageModel(BaseModel):
         if value == ImageFormatEnum.UNKNOWN:
             raise ValueError("Unknown image format")
         return value
+
+    @property
+    def image(self):
+        image_array = np.frombuffer(self.data, np.uint8)
+        # Decode the array into an image
+        image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+        return image
