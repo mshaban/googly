5033f26b106448a7700481c86d0e5b26baa0a15c
OPX <shaban@opx.slmail.me>
2024-03-18 19:51:26 +0100

    [🐛 FIXED:FEAT] Improve features model and image utils
    REFERENCES: [Face and Eye Detection](https://trello.com/c/mNouDMWu)

--- src/app/models/features.py
+++ src/app/models/features.py
@@ -1,67 +1,34 @@
-from pydantic import BaseModel, validator
-from src.app.core.logger import logger
 from abc import ABC
+
+from pydantic import BaseModel
 
 
 class FaceFeature(BaseModel, ABC):
-    x: int
-    y: int
-    w: int
-    h: int
+    xmin: int
+    ymin: int
+    xmax: int
+    ymax: int
 
     @property
     def coordinates(self):
-        return self.x, self.y, self.w, self.h
+        return self.xmin, self.ymin, self.xmax, self.ymax
+
+    @property
+    def width(self):
+        return ((self.xmax - self.xmin) ** 2) ** 0.5
+
+    @property
+    def height(self):
+        return ((self.ymax - self.ymin) ** 2) ** 0.5
+
+    @property
+    def size(self):
+        return max(self.width, self.height)
 
 
-class Face(FaceFeature):
+class FaceModel(FaceFeature):
     pass
 
 
-class Eye(FaceFeature):
+class EyeModel(FaceFeature):
     pass
-
-
-class GooglyFeatures(BaseModel):
-    face: Face
-    eyes: list[Eye]
-
-    # Validate that the eyes are within the face
-    # Validate that the eyes are max 2
-    # Validate that the eyes are not overlapping
-
-    @validator("eyes")
-    def validate_eyes(cls, eyes, values):
-        # Validate that the eyes are max 2
-        if len(eyes) > 2:
-            logger.error("There can be at most 2 eyes")
-            raise ValueError("There can be at most 2 eyes")
-        if len(eyes) >= 1:
-            logger.error("There must at least be 1 eye present")
-            raise ValueError("There must at least be 1 eye present")
-
-        # Validate that the eyes are not overlapping
-        for i in range(len(eyes)):
-            for j in range(i + 1, len(eyes)):
-                if (
-                    eyes[i].x < eyes[j].x + eyes[j].w
-                    and eyes[i].x + eyes[i].w > eyes[j].x
-                    and eyes[i].y < eyes[j].y + eyes[j].h
-                    and eyes[i].y + eyes[i].h > eyes[j].y
-                ):
-                    logger.error("Eyes are overlapping")
-                    raise ValueError("Eyes are overlapping")
-
-        # Validate eyes within the face.
-        face = values["face"]
-        for eye in eyes:
-            if (
-                eye.x < face.x
-                or eye.y < face.y
-                or eye.x + eye.w > face.x + face.w
-                or eye.y + eye.h > face.y + face.h
-            ):
-                logger.error("Eye is not within face")
-                raise ValueError("Eye is not within face")
-
-        return eyes
--- src/app/utils/image_utils.py
+++ src/app/utils/image_utils.py
@@ -1,9 +1,12 @@
+import cv2
 from fastapi import File, UploadFile
 import io
 
 from src.app.models.image import ImageModel
 from src.app.core.enums import ImageFormatEnum
 from src.app.core.logger import logger
+
+from src.app.models.features import FaceModel, EyeModel
 
 
 def save_image_from_bytes(image_bytes: bytes, file_path: str) -> None:
@@ -85,3 +88,83 @@ async def create_image_model(image: UploadFile = File(...)) -> ImageModel:
     )
     logger.debug(f"Image model created: {image_model.filename}")
     return image_model
+
+
+def image_path_to_bytes(image_path: str) -> bytes:
+    """
+    Reads an image file from the given path and returns its content as a bytes array.
+
+    :param image_path: The file system path to the image file.
+    :return: The image file's content as a bytes array.
+    """
+    with open(image_path, "rb") as image_file:
+        image_bytes = image_file.read()
+    return image_bytes
+
+
+def draw_annotations(
+    img, eye_coordinates: list[EyeModel], face_coordinates: list[FaceModel]
+):
+    """
+    Draw rectangles around detected faces and circles around detected eyes in the given image.
+
+    Parameters:
+    image_path (str): The file path of the image to process.
+    eye_coordinates (list[EyeModel]): A list of EyeModel objects containing the coordinates of detected eyes.
+    face_coordinates (list[FaceModel]): A list of FaceModel objects containing the coordinates of detected faces.
+
+    Returns:
+    None
+
+    Raises:
+    None
+
+    This function loads the image specified by image_path, draws rectangles
+    around the detected faces using the coordinates provided in
+    face_coordinates, and draws circles around the detected eyes using the
+    coordinates provided in eye_coordinates. The faces are outlined in green
+    and the eyes are circled in blue. The processed image is displayed in a
+    window titled "Faces and Eyes Detected" until a key is pressed, at which
+    point the window is closed.
+    """
+    # Draw rectangles around faces
+    for fc in face_coordinates:
+        xmin, ymin, xmax, ymax = fc.coordinates
+        cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)  # Draw in green
+
+    # # Draw circles around eyes
+    # for ey in eye_coordinates:
+    #     xmin, ymin, xmax, ymax = ey.coordinates
+    #
+    #     center = (xmin + (xmax - xmin) // 2, ymin + (ymax - ymin) // 2)
+    #     radius = max(xmax - xmin, ymin - ymax) // 5
+    #     cv2.circle(img, center, radius, (255, 0, 0), 2)  # Draw in blue
+    visualize_landmarks(img, eye_coordinates)
+    # Display the image
+    cv2.imshow("Faces and Eyes Detected", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def visualize_landmarks(image, landmarks):
+    """
+    Visualizes facial landmarks on the given image.
+
+    Parameters:
+    image (numpy.ndarray): The input image on which to visualize the landmarks.
+    results (numpy.ndarray): The array of facial landmarks detected in the image.
+
+    Returns:
+    None
+
+    Raises:
+    None
+    """
+    for l in landmarks:
+        x, y = l.xmin, l.ymin
+        cv2.circle(image, (x, y), 2, (0, 255, 0), -1)
+        x, y = l.xmax, l.ymax
+        cv2.circle(image, (x, y), 2, (0, 255, 0), -1)
+    # cv2.imshow("Facial Landmarks", image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
