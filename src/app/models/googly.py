b496750e3de41bc083016e499bd026e481f1d4c2
OPX <shaban@opx.slmail.me>
2024-03-20 18:26:32 +0100

    fixup! [♻️ REFACTOR:CONFIG] remove unwanted functions

--- src/app/models/googly.py
+++ src/app/models/googly.py
@@ -11,51 +11,6 @@ class GooglyModel(BaseModel):
     input_image: ImageModel
     googly: ImageModel
 
-    @model_validator(mode="before")
-    def validate_eyes_faces(cls, values):
-        """
-        Validates the relationship between eyes and faces in a given set of values.
-
-        Parameters:
-        cls (class): The class to which this method belongs.
-        values (dict): A dictionary containing the values to be validated, with keys "eyes" and "faces".
-
-        Returns:
-        dict: The validated values dictionary.
-
-        Raises:
-        ValueError: If the number of eyes is not twice the number of faces, or if any pair of eyes is not within their respective face.
-        """
-
-        eyes, faces = values["eyes"], values["faces"]
-
-        # Check if the number of eyes is twice the number of faces
-        if len(eyes) != 2 * len(faces):
-            logger.error(f"eyes: {len(eyes)} faces: {len(faces)}")
-            raise ValueError("Mismatch between number of eyes and faces")
-
-        # Validate that each pair of eyes is within their respective face in the list
-        for i in range(0, len(eyes) - 1, 2):
-            face_model = faces[i // 2]
-            left_eye, right_eye = eyes[i], eyes[i + 1]
-
-            # Check if both eyes are within the boundaries of the face
-            if not all(
-                [
-                    left_eye.points[0].x >= face_model.xmin,
-                    left_eye.points[0].y >= face_model.ymin,
-                    left_eye.points[1].x <= face_model.xmax,
-                    left_eye.points[1].y <= face_model.ymax,
-                    right_eye.points[0].x >= face_model.xmin,
-                    right_eye.points[0].y >= face_model.ymin,
-                    right_eye.points[1].x <= face_model.xmax,
-                    right_eye.points[1].y <= face_model.ymax,
-                ]
-            ):
-                raise ValueError("Eyes are not within the face")
-
-        return values
-
     def model_post_init(self, *args, **kwargs):
         self.eyes = [
             EyeModel(points=self.normalize_eye_points(eye)) for eye in self.eyes
